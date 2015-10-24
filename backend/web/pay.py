63321253892bea5796e542812affbee296e705f8
Tocknicsu <wingemerald@gmail.com>
2015-10-24 23:09:34 +0800

    update

--- backend/web/pay.py
+++ backend/web/pay.py
@@ -5,6 +5,9 @@ import tornado
 class WebPayHandler(WebRequestHandler):
     @tornado.gen.coroutine
     def get(self, qrcode):
-        print(qrcode)
+        self.render('pay/check.html', qrcode=qrcode)
+    
+    @tornado.gen.coroutine
+    def post(self, qrcode):
         err, res = yield from Service.Pay.payqr({'token': self.token, 'id': self.id, 'token': self.token, 'qrcode': qrcode})
 
