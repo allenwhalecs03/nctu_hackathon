d18a3eb0ebf465a29eae9cf1d1f7cea574ab707e
allenwhale <allenwhale@yahoo.com.tw>
2015-10-24 15:17:37 +0800

    update

--- backend/service/user.py
+++ backend/service/user.py
@@ -14,16 +14,23 @@ class UserService(BaseService):
         '''
         url = self.add_client_id(config.BASE_URL + '/login') 
         r = requests.post(url, data=json.dumps(data))
-        try: res = json.loads(r.text)
-        except: res = {'message': r.text}
-        token = res.get('token')
-        if token: req.set_secure_cookie('token', token)
-        return (None if token else res['message'], token)
+        try: 
+            res = json.loads(r.text)
+            req.set_secure_cookie('token', res['token'])
+            _, res_cnt = yield from self.db.execute('SELECT * FROM users WHERE username = %s;', (data['username'],))
+            if res_cnt == 0:
+                insert_data = {'username': data['username'], 'account_id': res['account_id']}
+                sql, param = self.gen_insert_sql('users', insert_data)
+                yield from self.db.execute(sql, param)
+            return (None, token)
+        except: 
+            res = {'message': r.text}
+            return (r.text, None)
 
     def get_user_info(self, token):
         url = self.add_client_id(config.BASE_URL + '/accounts')
         r = requests.get(url, headers=self.headers(token))
         try: 
-            return (None, json.loads(r.text))
+            return (None, json.loads(r.text)['accounts'][0])
         except:
             return (r.text, None)
