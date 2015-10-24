e2b70866d93a1d88d47fdf07a9d684cdcbf09a32
Tocknicsu <wingemerald@gmail.com>
2015-10-24 10:30:30 +0800

    update

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,11 @@
 import datetime
 PASSWORD_SALT = ''
-DBNAME = 'nctuoj'
-DBUSER = 'nctuoj'
-DBPASSWORD = 'yavaf2droyPo'
+DBNAME = 'hackathon'
+DBUSER = 'hackathon'
+DBPASSWORD = 'hackathon'
 DBHOST = '140.113.178.132'
-COOKIE_SECRET = 'wef wqfqw wqefwqf2 '
-PORT = '3018'
+COOKIE_SECRET = 'wef wqasdfsdffqw wqefwqf2 '
+PORT = '1078'
 MAX_WAIT_SECOND_BEFORE_SHUTDOWN = 2
 LOG_FILE_PATH = './log/server.log'
 DATA_EXPIRE_SECOND = 86400
