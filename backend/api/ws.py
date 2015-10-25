0c7afdc05568bee354eae9de4b4806b85e1d781e
allenwhale <allenwhale@yahoo.com.tw>
2015-10-25 11:16:22 +0800

    update

--- backend/api/ws.py
+++ backend/api/ws.py
@@ -10,17 +10,18 @@ import tornado
 class SocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
-
+    @tornado.gen.coroutine
     def open(self):
-        self.ars = tornadoredis.Client(select_db = 2)
+        print('open')
+        self.ars = tornadoredis.Client(selected_db = 2)
         self.ars.connect()
         yield tornado.gen.Task(self.ars.subscribe, 'pay_list')
-        self.ars.listen(on_message)
+        self.ars.listen(self.on_message)
 
     def on_message(self, msg):
         if msg.kind == 'message':
             print(type(msg.body), msg.body)
-            self.write_message(str(msg))
+            self.write_message(str(msg.body))
 
     def on_close(self):
         pass
