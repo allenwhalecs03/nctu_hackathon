62f4421ee77d20a9a2e158b8f824c3e4354e6a4b
allenwhale <allenwhale@yahoo.com.tw>
2015-10-25 10:55:25 +0800

    update

--- backend/api/ws.py
+++ backend/api/ws.py
@@ -3,27 +3,25 @@ import json
 import time
 from req import ApiRequestHandler
 from req import Service
-#Service.User.get_user_info(self.token)
-c1 = []
+import tornadoredis
+import tornado
 
-def chk(ws):
-    pass
+
 class SocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
 
     def open(self):
-        print(Service.User.get_user_info(self.token))
-        #if not check_client_exist(self):
-        #    data = json.dumps(data)
-        #    self.write_message(data)
-        #    cl.append(client)
+        self.ars = tornadoredis.Client(select_db = 2)
+        self.ars.connect()
+        yield tornado.gen.Task(self.ars.subscribe, 'pay_list')
+        self.ars.listen(on_message)
 
-    def on_message(self, data):
-        client.write_message("hello")
+    def on_message(self, msg):
+        if msg.kind == 'message':
+            print(type(msg.body), msg.body)
+            self.write_message(str(msg))
 
     def on_close(self):
         pass
 
-    def send_device_list(self):
-        pass
